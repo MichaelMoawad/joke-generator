2834cfcd7cd6be5f45239b5d257ea38c7eea401f
Michael Moawad <michaelmoawad1997@gmail.com>
2022-10-17 14:35:50 +0100

    First Joke Generator commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import headerImage from './Joke.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ 
+  constructor(props) {
+      super(props)
+
+      this.state = {
+        joke: "",
+        badJokes: [],
+        goodJokes: [],
+
+      }
+      this.getJoke = this.getJoke.bind(this);
+      this.notFunny = this.notFunny.bind(this);
+      this.prettyFunny = this.prettyFunny.bind(this);
+  }
+
+  getJoke(){
+    const baseURL = `https://geek-jokes.sameerkumar.website/api?format=json`
+      axios.get(baseURL).then((res) => {
+        this.setState({joke: res.data.joke})
+        console.log("hi")
+      })
+  }
+
+  notFunny(){
+    if((this.state.joke != this.state.badJokes[this.state.badJokes.length -1]) && (this.state.joke != this.state.goodJokes[this.state.goodJokes.length -1])){
+      this.state.badJokes.push(this.state.joke)
+      console.log(this.state.badJokes)
+      this.forceUpdate()
+    }
+  }
+
+  prettyFunny(){
+    if((this.state.joke != this.state.goodJokes[this.state.goodJokes.length -1]) && (this.state.joke != this.state.badJokes[this.state.badJokes.length -1])){
+      this.state.goodJokes.push(this.state.joke)
+      console.log(this.state.goodJokes)
+      this.forceUpdate()
+    }
+  }
+
+  
+  componentDidMount(){
+    this._Mounted = true;
+    if (this._Mounted){
+      this.getJoke()
+    }
+  }
+
+  componentDidUpdate(){
+    
+  }
+
+  componentWillUnmount(){
+
+  }
+
+  render() {
+
+    let goodJokesList = <div className='good-jokes'>
+      {this.state.goodJokes.slice(0).reverse().map(item => <div className='text-padding'><p>{item}</p></div>
+      )}
+    </div>;
+
+    let badJokesList = <div className='bad-jokes'>
+      {this.state.badJokes.slice(0).reverse().map(item => <div className='text-padding'><p>{item}</p></div>
+      )}
+    </div>;
+
+      return (
+        <div className='main-container'>
+          <div className='action-container'>
+            <h1>Want to hear a funny Joke?</h1>
+            {/* <img src={headerImage}/> */}
+            <div className='button-container-joke'>
+            <button class="pushable" onClick={this.getJoke}>
+              <span class="shadow"></span>
+              <span class="edge"></span>
+              <span class="front">
+                Generate
+              </span>
+            </button>
+            </div>
+            <div class="slideContainer">
+              <div class="bubble"></div>
+              <div class="slider">
+                  <div class="slide"><p className='joke-text'>{this.state.joke}</p></div>
+              </div>
+            </div>
+            {/* <p className='joke-text'>{this.state.joke}</p> */}
+          </div>
+
+          <div className='results-container'>
+
+            <div className='goodresults-container'>
+              <div className='button-container'>
+              {/* <button onClick={this.prettyFunny}> Hilarious! </button> */}
+              <button class="pushable" onClick={this.prettyFunny}>
+                <span class="shadow"></span>
+                <span class="edge"></span>
+                <span class="front">
+                  Hilarious!
+                </span>
+              </button>
+              </div>
+                {goodJokesList}
+            </div>
+
+            <div className='badresults-container'>
+              <div className='button-container'>
+              {/* <button onClick={this.notFunny}> Not funny bro </button> */}
+              <button class="pushable" onClick={this.notFunny}>
+                <span class="shadow"></span>
+                <span class="edge"></span>
+                <span class="front">
+                  Not funny
+                </span>
+              </button>
+              </div>
+                {badJokesList}
+            </div>
+          </div>
+
+        </div>
+      );
+    }
 }
 
-export default App;
+export default App
+
